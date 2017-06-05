d3c0d1b38aaa2d4ded8ad7ed65e6ca0ed8e5f5b4
Math-Man <kayacan.goktug@gmail.com>
2017-06-05 19:35:10 +0300

    Collatz Tag System
    An attempt to create a collatz system using Dictionaries and Queues.
    
    https://en.wikipedia.org/wiki/Tag_system#Example:_Computation_of_Collatz_sequences

---
+++ CollatzTagSystem/CollatzTagSystem/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//Collatz tag system 05/06/2017
+//https://en.wikipedia.org/wiki/Tag_system#Example:_Computation_of_Collatz_sequences
+//Uses C# Queues and Dictionries
+
+namespace CollatzTagSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, char[]> alphabet = new Dictionary<char, char[]>
+            {
+                { 'a', new char[] { 'b','c' } },
+                { 'b', new char[] { 'a' } },
+                { 'c', new char[] { 'a', 'a', 'a' } }
+            };
+            Console.Write("Input should be something similar to aaa, aaaa, aaabc , aaabbc etc.\n");
+            string inputLine = Console.ReadLine(); //example : aaa
+            string currentLine = inputLine;
+            Queue<char> seq = new Queue<char>();
+
+            for (int i = 0; i < currentLine.Length; i++)
+            {
+                seq.Enqueue(currentLine[i]);
+            }
+
+            while ((currentLine.Length > 2))
+            {
+
+                char c = seq.Dequeue();
+                if (seq.Count == 0) { Console.ReadKey(); return; }
+                seq.Dequeue();
+
+
+                foreach (char s in alphabet[c])
+                {
+                    seq.Enqueue(s);
+                }
+
+                Console.WriteLine(seq.ToArray<char>());
+
+            }
+
+        }
+    }
+}
