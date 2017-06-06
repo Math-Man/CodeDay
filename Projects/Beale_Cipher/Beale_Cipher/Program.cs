60e3214572c35c24b003d9966bf050f3724f2ac0
Math-Man <kayacan.goktug@gmail.com>
2017-06-06 13:28:50 +0300

    Beale Cipher
    Modified from an example on:
    https://www.reddit.com/r/dailyprogrammer/comments/4r8fod/20160704_challenge_274_easy_gold_and_treasure_the/

---
+++ Projects/Beale_Cipher/Beale_Cipher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Int32;
+using static System.IO.File;
+
+
+namespace Beale_Cipher
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> l = new List<int>();
+            l = ReadAllText("Cipher.txt").Split(',').Select(Parse).ToList();
+
+            l.ForEach(c => Write($"{ReadAllText("DOI.txt").Split(' ')[c - 1][0]}"));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Projects/Resistance Calculator/Resistance Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Program to calculate resistances between 2 points 05/06/2017
+// www.reddit.com/r/dailyprogrammer/comments/4jx7y8/20160518_challenge_267_intermediate_vive_la/
+
+/*
+ Example input:
+ A B C
+ A B 10
+ A B 30
+ B C 50
+
+    Input always starts with node A
+*/
+
+namespace Resistance_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+           
+
+        }
+    }
+
+    class node
+    {
+
+        public double v { get; set; }
+        public int fix { get; set; }
+
+    }
+
+}
