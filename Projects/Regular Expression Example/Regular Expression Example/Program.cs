57a5377031b272df24048cfe48c5e81ebc7645bb
Math-Man <kayacan.goktug@gmail.com>
2017-06-09 21:17:28 +0300

    Regex Example
    This is a modified version of an old code I saved, I couldn't find the
    original :( .

---
+++ Projects/Regular Expression Example/Regular Expression Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+/*
+
+    This app demonstrates regular expression.
+    I just happend to learn about Regex library and it seems so much fun.
+
+    
+     
+*/
+namespace Regular_Expression_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string input = Console.ReadLine();
+            Regex r = new Regex("(?i)[b-df-hj-np-tv-xz]");//non vowels
+            Console.WriteLine(r.Replace(input, new MatchEvaluator(rob)));
+
+            Console.ReadKey();
+        }
+
+        static string rob(Match m) { return m.Value + "o" + m.Value.ToLower(); }
+    }
+}
