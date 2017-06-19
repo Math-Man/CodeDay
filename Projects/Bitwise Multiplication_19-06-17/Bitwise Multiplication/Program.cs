d8f4c6ced483036f7caa78a4f538b592774b0ae5
Math-Man <kayacan.goktug@gmail.com>
2017-06-19 20:34:51 +0300

    Bitwise Multiplication
    Very basic demonstration of bitwise operations.

---
+++ Projects/Bitwise Multiplication_19-06-17/Bitwise Multiplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*This is part of a project where I attempt to code something intresting everyday*/
+namespace Bitwise_Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, fin;
+
+            /*Inputs*/
+            a = 4;
+            b = 3;
+            /*      */
+            fin = 0;
+
+            while (b != 0)
+            {
+                if ((b & 01) == 1)  //bitwise AND
+                {
+                    fin += a;
+                }
+                a <<= 1; //left shit a by 1
+                b >>= 1; //Right shift b by 1
+            }
+
+            Console.WriteLine("R: " + fin);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projects/RotatedStrings/RotatedStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotatedStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            List<char> output = new List<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == 0) { output[input.Length] = input[0]; }
+                else
+                {
+                    output[i + 1] = input[i];
+                }
+            }
+
+
+            foreach ()
+            {
+
+            }
+
+
+        }
+    }
+}
