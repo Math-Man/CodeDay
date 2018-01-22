2e7a1a306cbae9edaecf05c22518d7ff2d93aa36
Math-Man <kayacan.goktug@gmail.com>
2018-01-22 17:42:41 +0300

    Random Polygon Drawer

---
+++ Projects/RandomPolygon_22_01_17/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace RandomPolygon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Draw = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Draw
+            // 
+            this.Draw.Location = new System.Drawing.Point(96, 240);
+            this.Draw.Name = "Draw";
+            this.Draw.Size = new System.Drawing.Size(75, 23);
+            this.Draw.TabIndex = 0;
+            this.Draw.Text = "Draw";
+            this.Draw.UseVisualStyleBackColor = true;
+            this.Draw.Click += new System.EventHandler(this.Draw_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(8, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 224);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 273);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Draw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Draw;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Projects/RandomPolygon_22_01_17/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomPolygon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            Bitmap bm = drawRandomPolyAdvanced(120, 120, rnd.Next(60), rnd.Next(55), rnd.Next(60), rnd.Next(55));
+            pictureBox1.Image = bm;
+        }
+
+        public static Bitmap drawRandomPolyAdvanced(int h, int k, int rx, int ry, int rx2, int ry2)
+        {
+            Random rnd = new Random();
+            Bitmap bmp = new Bitmap(720, 720);
+            Graphics g = Graphics.FromImage(bmp);
+            Pen p = new Pen(Color.Red, 2);
+            List<Point> points = new List<Point>();
+
+            double step = 2 * Math.PI / 5;
+
+            for (double angle = 0; angle < 2 * Math.PI; angle += step)
+            {
+                double x;
+                double y;
+                if (rnd.NextDouble() > 0.5)
+                {
+                    x = h + rx * Math.Cos(angle);
+                    y = k - ry * Math.Sin(angle);
+                }
+                else
+                {
+                    x = h + rx2 * Math.Cos(angle);
+                    y = k - ry2 * Math.Sin(angle);
+                }
+
+                points.Add(new Point((int)x, (int)y));
+                step = 2 * Math.PI / rnd.Next(5, 20);
+                if (angle + step > 2 * Math.PI)
+                {
+                    angle = 2 * Math.PI;
+                }
+            }
+
+            g.DrawPolygon(p, points.ToArray());
+
+            bmp.Save("out.png", ImageFormat.Png);
+            return bmp;
+        }
+    }
+}
